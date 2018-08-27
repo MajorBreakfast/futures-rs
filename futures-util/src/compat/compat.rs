2031fca0aaa5ea65e3b8438d2e6a021f7da55c61
Josef Reinhard Brandl <mail@josefbrandl.de>
2018-08-27 09:26:12 +0200

    Remove Sink from compat description

--- futures-util/src/compat/compat.rs
+++ futures-util/src/compat/compat.rs
@@ -1,9 +1,7 @@
-/// Converts a futures 0.3 [`TryFuture`](futures_core::future::TryFuture),
-/// [`TryStream`](futures_core::stream::TryStream) or
-/// [`Sink`](futures_sink::Sink) into a futures 0.1
-/// [`Future`](futures01::future::Future),
-/// [`Stream`](futures01::stream::Stream) or
-/// [`Sink`](futures01::sink::Sink) and vice versa.
+/// Converts a futures 0.3 [`TryFuture`](futures_core::future::TryFuture) or
+/// [`TryStream`](futures_core::stream::TryStream) into a futures 0.1
+/// [`Future`](futures01::future::Future) or
+/// [`Stream`](futures01::stream::Stream) and vice versa.
 #[derive(Debug)]
 #[must_use = "futures do nothing unless polled"]
 pub struct Compat<T, Sp> {
